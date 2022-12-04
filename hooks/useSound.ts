f2d624fb6166df666e83c30a58f2dd8fc0964360
Ramone Henry <ramoneh94@gmail.com>
2022-12-04 10:30:53 -0500

    remove comments in useSound file

--- hooks/useSound.ts
+++ hooks/useSound.ts
@@ -1,22 +1,6 @@
 import { useCallback } from 'react'
 import TrackPlayer from 'react-native-track-player'
 
-/*
-
-Old code to be kept for reference and to use as a back up
-  const playSpace = () => {
-    TrackPlayer.skip(0)
-    TrackPlayer.play()
-    toggleSpaceState()
-    if (spaceSoundStatus) {
-      TrackPlayer.pause()
-    }
-    if (!isPlaying) {
-      onToggle()
-    }
-  }
-*/
-
 export const useSound = (
   soundStatus: boolean,
   playingStatus: boolean,
