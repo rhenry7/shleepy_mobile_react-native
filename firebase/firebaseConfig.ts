7d6e67101fa5b295c9cfe3494dc9f258732ffab6
Ramone Henry <ramoneh94@gmail.com>
2022-12-04 23:56:17 -0500

    installed firebase to project and set up config file

---
+++ firebase/firebaseConfig.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBmQHTqztzPW2q-odOnktuwNV42cW9y1A8',
+  authDomain: 'shleepy-app-react-native.firebaseapp.com',
+  projectId: 'shleepy-app-react-native',
+  storageBucket: 'shleepy-app-react-native.appspot.com',
+  messagingSenderId: '738770230249',
+  appId: '1:738770230249:web:71ffac368ca8844e484978',
+  measurementId: 'G-ETSGJCG0J1',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+// const analytics = getAnalytics(app);
