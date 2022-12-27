a799d94523b70d7fb0d8ec80d2be57b844c5dd6d
Ramone Henry <ramoneh94@gmail.com>
2022-12-27 00:05:56 -0500

    verified that firebase can be used to access/download url for sound files in storage

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -2,7 +2,7 @@
 import { getApp, initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { GoogleAuthProvider } from 'firebase/auth'
-import { getStorage, ref } from 'firebase/storage'
+import { getDownloadURL, getStorage, ref } from 'firebase/storage'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,7 +13,7 @@ const firebaseConfig = {
   apiKey: 'AIzaSyBmQHTqztzPW2q-odOnktuwNV42cW9y1A8',
   authDomain: 'shleepy-app-react-native.firebaseapp.com',
   projectId: 'shleepy-app-react-native',
-  storageBucket: 'shleepy-app-react-native.appspot.com',
+  storageBucket: 'gs://shleepy-app-react-native.appspot.com',
   messagingSenderId: '738770230249',
   appId: '1:738770230249:web:71ffac368ca8844e484978',
   measurementId: 'G-ETSGJCG0J1',
@@ -32,10 +32,25 @@ export const auth = getAuth(app)
 
 // Initialize Cloud Storage and get a reference to the service
 const storage = getStorage(app)
+console.log({ storage })
 
 // Create a storage reference from our storage service
-export const storageRef = ref(storage)
+export const storageRef = ref(storage, 'sounds')
+console.log({ storageRef })
 
+const wind = getDownloadURL(ref(storage, 'sounds/nature/wind.wav'))
+  .then((url) => {
+    console.log('even being hit?!')
+    console.log(url)
+    return url
+    // Or inserted into an <img> element
+  })
+  .catch((error) => {
+    console.log(error)
+    // Handle any errors
+  })
+
+console.log('hello?!!')
 export const provider = new GoogleAuthProvider()
 provider.addScope('https://www.googleapis.com/auth/contacts.readonly')
 
--- sounds/Tracks.ts
+++ sounds/Tracks.ts
@@ -1,3 +1,11 @@
+// import { getStorage, ref, getDownloadURL } from 'firebase/storage'
+
+// const storage = getStorage()
+// getDownloadURL(ref(storage)).then((url) => {
+//   const soundFileLocations = url
+//   console.log({ soundFileLocations })
+// })
+
 export const tracks = [
   {
     id: 1,
@@ -7,6 +15,7 @@ export const tracks = [
   {
     id: 2,
     url: require('./nature/wind.wav'),
+    // url: https://firebasestorage.googleapis.com/v0/b/shleepy-app-react-native.appspot.com/o/sounds%2Fnature%2Fwind.wav?alt=media&token=8dbfa8ee-3aea-4e98-8e22-35becc67d964
     title: 'Blues Beat',
   },
   {
